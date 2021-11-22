47b562f65e7f595f74ad9f369f39c0d56bd89c9e
toame <tomo2073_41@yahoo.co.jp>
2021-11-22 22:21:14 +0900

    heatmapスクリプト作成

---
+++ reservoir_ESN/script/heatmap.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import os
+import random
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm
+folder = "../output_data/"
+
+
+data = np.random.uniform(0, 1.0, (20, 20))
+row_labels = range(0, 40, 2)
+column_labels = range(0, 20, 1)
+
+for name in os.listdir(folder):
+    print(name)
+    name = os.path.splitext(os.path.basename(name))[0]
+    df = pd.read_csv(folder + name + '.csv', sep=',',comment='#')
+    function_names = ["tanh", "sinc"]
+    task_name = "approx1_1.0"
+    
+    for function_name in function_names:
+        df_sub = df.query('function_name == @function_name')
+        df_sub = df_sub[df_sub[task_name] < 1.1]
+        for i in range(20):
+            for j in range(20):
+                L_lower = i * 2
+                L_upper = (i + 1) * 2
+                NL_lower = j * 1
+                NL_upper = (j + 1) * 1
+                df_sub2 = df_sub.query('@NL_lower <= NLx_3 & NLx_3 < @NL_upper & @L_lower <= Lx_4 & Lx_4 < @L_upper')
+                if len(df_sub2) > 0:
+                    mean = df_sub2[task_name].mean()
+                else:
+                    mean = 1.0
+                #print(i, j, mean, len(df_sub2))
+                data[j, i] = mean
+        print(data)
+        fig, ax = plt.subplots()
+        heatmap = ax.pcolor(data, cmap=plt.cm.Blues)
+
+        ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+        ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+
+        ax.invert_yaxis()
+        ax.xaxis.tick_top()
+
+        ax.set_xticklabels(row_labels, minor=False)
+        ax.set_yticklabels(column_labels, minor=False)
+        #plt.show()
+        plt.savefig('image_' + function_name + '.png', dpi = 200)
+        plt.clf()
