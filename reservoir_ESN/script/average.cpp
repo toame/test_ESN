9450637650408b5464672c58c698ba3936bee0c6
toame <tomo2073_41@yahoo.co.jp>
2022-03-03 17:26:59 +0900

    解析可能なタスク数を可変にする

--- reservoir_ESN/script/average.cpp
+++ reservoir_ESN/script/average.cpp
@@ -9,11 +9,11 @@
 #include <algorithm>
 using namespace std;
 
-const double max_L = 50;
-const double max_NL = 100;
-const int splitL_C = 25;
-const int splitNL_C = 20;
-const string NLtype = "NL1_old";
+const double max_L = 100;
+const double max_NL = 500;
+const int splitL_C = 50;
+const int splitNL_C = 50;
+const string NLtype = "NL_old";
 const double split_L = max_L/splitL_C;
 const double split_NL = max_NL/splitNL_C;
 map<string, int> mp;
@@ -55,47 +55,53 @@ double calc_SD(std::vector<double>& data)
 int main (void) {
     string root_path = "../output_data/";
     vector<string> pathes{"NL_0_0.0_100_random"};
+    vector<string> task{"approx_3_0.0", "approx_6_-0.5", "approx_11_-1.0"};
     for(auto path: pathes) {
         vector<vector<string>> data = input_csv(root_path + path + ".csv");
-        vector<double> nmse1(splitL_C * splitL_C), nmse2(splitL_C * splitL_C), nmse3(splitL_C * splitL_C);
-        vector<vector<double>> nmse1_vec(splitL_C * splitL_C), nmse2_vec(splitL_C * splitL_C), nmse3_vec(splitL_C * splitL_C);
+        map<string, vector<double>> nmse;
+        map<string, vector<vector<double>>> nmse_vec;
+        for(auto task_name: task) {
+            nmse[task_name].resize((splitL_C * splitL_C));
+            nmse_vec[task_name].resize(splitL_C * splitL_C);
+        }
         vector<int> nmse_cnt(splitL_C * splitL_C);
         for(int r = 1; r < data.size(); r++) {
             vector<string> elements = data[r];
             int NLr = stod(elements[mp[NLtype]]) / split_NL;
             int Lr = stod(elements[mp["L"]]) / split_L;
-            const double tmp_nmse1 = stod(elements[mp["approx_3_0.0"]]);
-            const double tmp_nmse2 = stod(elements[mp["approx_6_-0.5"]]);
-            const double tmp_nmse3 = stod(elements[mp["approx_11_-1.0"]]);
-            if (tmp_nmse1 > 1.05 || tmp_nmse2 > 1.05 || tmp_nmse3 > 1.05) 
-                continue;
-            nmse1[NLr * splitL_C + Lr] += tmp_nmse1;
-            nmse2[NLr * splitL_C + Lr] += tmp_nmse2;
-            nmse3[NLr * splitL_C + Lr] += tmp_nmse3;
-            nmse1_vec[NLr * splitL_C + Lr].push_back(tmp_nmse1);
-            nmse2_vec[NLr * splitL_C + Lr].push_back(tmp_nmse2);
-            nmse3_vec[NLr * splitL_C + Lr].push_back(tmp_nmse3);
+            
             nmse_cnt[NLr * splitL_C + Lr]++;
         }
-        vector<string> task{"approx_3_0.0", "approx_6_-0.5", "approx_11_-1.0"};
-        for(int id = 0; id < 3; id++) {
-            std::ofstream ofs_csv_file(root_path + path + "_" + task[id] + "_" + NLtype + "_average.csv");
+        
+        for(int r = 1; r < data.size(); r++) {
+            for(auto task_name: task) {
+                vector<string> elements = data[r];
+                vector<double>& nmse_task = nmse[task_name];
+                vector<vector<double>>& nmse_vec_task = nmse_vec[task_name];
+                int NLr = stod(elements[mp[NLtype]]) / split_NL;
+                int Lr = stod(elements[mp["L"]]) / split_L;
+                const double tmp_nmse = stod(elements[mp[task_name]]);
+                if (tmp_nmse > 1.05) 
+                    continue;
+                nmse_task[NLr * splitL_C + Lr] += tmp_nmse;
+                nmse_vec_task[NLr * splitL_C + Lr].push_back(tmp_nmse);
+            }
+        }
+        for(auto task_name: task){
+            std::ofstream ofs_csv_file(root_path + path + "_" + task_name + "_" + NLtype + "_average.csv");
             cerr << root_path + path + "_average.csv" << endl;
             ofs_csv_file << "L,NL,nmse" << endl;
             for(int i = 0; i < splitL_C; i++) {
                 for(int j = 0; j < splitNL_C; j++) {
                     int idx = i * splitL_C + j;
                     if(nmse_cnt[idx]) {
-                        if(id == 0)
-                            ofs_csv_file << split_L * j << "," << split_NL * i << "," <<nmse1[idx]/nmse_cnt[idx] << "," << nmse_cnt[idx] << "," << calc_SD(nmse1_vec[idx]) << endl;
-                        if(id == 1)
-                            ofs_csv_file << split_L * j << "," << split_NL * i << "," <<nmse2[idx]/nmse_cnt[idx] << "," << nmse_cnt[idx] << "," << calc_SD(nmse2_vec[idx]) << endl;
-                        if(id == 2)
-                            ofs_csv_file << split_L * j << "," << split_NL * i << "," <<nmse3[idx]/nmse_cnt[idx] << "," << nmse_cnt[idx] << "," << calc_SD(nmse3_vec[idx]) << endl;
+                        ofs_csv_file << split_L * j << "," 
+                                     << split_NL * i << "," 
+                                     << nmse[task_name][idx] / nmse_cnt[idx] << "," 
+                                     << nmse_cnt[idx] << "," << calc_SD(nmse_vec[task_name][idx]) << endl;
                     }
                 }
             }
         }
     }
-
 }
