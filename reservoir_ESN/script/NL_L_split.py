f55883d4c4d433718da80436d051fe24eb35e0fc
toame <tomo2073_41@yahoo.co.jp>
2022-03-04 18:00:03 +0900

    新しい入力形式に対応する

--- reservoir_ESN/script/NL_L_split.py
+++ reservoir_ESN/script/NL_L_split.py
@@ -4,15 +4,15 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 import math
 #for path in ["NL_0_0.0_50_ring","NL_0_0.0_50_random","NL_0_0.0_100_ring","NL_0_0.0_100_random"]:
-for path in ["NL_0_0.0_100_random", "NL_0_0.0_100_ring"]:
-    NLtype = "NL1_old"
+for path in ["NL_0_0.0_100_random"]:
+    NLtype = "NL_test"
     for task in ["approx_3_0.0","approx_6_-0.5","approx_11_-1.0"]:
-        df = pd.read_csv('C:\\Users\\pc07-mis\\Desktop\\test_ESN\\reservoir_ESN\\output_data\\' + path + '_' + task + "_" + NLtype + '_average.csv', sep=',',comment='#');
+        df = pd.read_csv('C:\\Users\\tomo2\\Desktop\\test_ESN\\reservoir_ESN\\output_data\\' + path + "_" + NLtype + '_average.csv', sep=',',comment='#');
         data_x = df['L'] + 1.0
-        data_y = df['NL'] + 1.0
-        value = df['nmse']
+        data_y = df['NL'] + 5.0
+        value = df[task + '_nmse']
         plt.scatter(data_x, data_y,s=20, marker="s", c = value, alpha=1, linewidths=1, norm=LogNorm(vmin=0.05, vmax=1.0), cmap='viridis_r')
-        plt.ylim(0.0, 100)
+        plt.ylim(0.0, 500)
         plt.xlim(0.0, 100)
         #plt.legend(loc = "best")
         plt.colorbar()
