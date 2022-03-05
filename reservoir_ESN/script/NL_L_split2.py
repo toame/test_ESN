ec80c96efe6a9fd64862c70838fd6591889cb12e
toame <tomo2073_41@yahoo.co.jp>
2022-03-05 18:50:27 +0900

    複数グラフを生成

--- reservoir_ESN/script/NL_L_split2.py
+++ reservoir_ESN/script/NL_L_split2.py
@@ -14,17 +14,29 @@ args = parser.parse_args()
 for path in args.data:
     for NL_type in args.NL_types:
         for task in args.taskes:
+            fig = plt.figure()
+            ax1 = fig.add_subplot(1, 4, 1)
+            ax2 = fig.add_subplot(1, 4, 2)
+            ax3 = fig.add_subplot(1, 4, 3)
+            ax4 = fig.add_subplot(1, 4, 4)
             df = pd.read_csv(args.root + path + '.csv', sep=',',comment='#')
-            df = df[df["function_name"] == "sinc"]
-            #df = df[df["bias_factor"] != 0]
-            #df2.to_csv(task + ".csv")
-            data_x = df['L_cut']
-            data_y = df[NL_type]
-            value = df[task]
-            plt.scatter(data_x, data_y,s=3, marker="o", c = value, alpha=1, linewidths=1, norm=LogNorm(vmin=0.05, vmax=1.0), cmap='viridis_r')
-            plt.ylim(0.0, 500)
-            plt.xlim(0.0, 50)
-            #plt.legend(loc = "best")
-            plt.colorbar()
-            plt.savefig(args.output + path + "_" + task + "_" + NL_type + ".png", dpi = 600)
-            plt.clf()
+            for function_name, bias, ax in [("sinc", True, ax1), ("tanh", True, ax2), ("sinc", False, ax3),("tanh", False, ax4)]:
+                print(path, NL_type, task)
+                df2 = df[df["function_name"] == function_name]
+                if bias:
+                    df2 = df2[df2["bias_factor"] != 0]
+                else:
+                    df2 = df2[df2["bias_factor"] == 0]
+                #df2.to_csv(task + ".csv")
+                print(df2)
+                data_x = df2['L_cut']
+                data_y = df2[NL_type]
+                value = df2[task]
+                mappable = ax.scatter(data_x, data_y,s=1, marker=".", c = value, alpha=1, linewidths=1, norm=LogNorm(vmin=0.05, vmax=1.0), cmap='viridis_r')
+                ax.set_ylim(0.0, 500)
+                ax.set_xlim(0.0, 50)
+                ax.set_title(function_name + "," + str(bias))
+                #plt.legend(loc = "best")
+            fig.colorbar(mappable, ax=ax)
+            fig.savefig(args.output + path + "_" + task + "_" + NL_type + ".png", dpi = 600)
+            fig.clf()
