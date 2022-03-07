78d91982d1f3934ac6c6dcc8065668b4542e1aea
toame <tomo2073_41@yahoo.co.jp>
2022-03-07 21:22:02 +0900

    全ての点を表示するグラフを追加

--- reservoir_ESN/script/NL_L_split2.py
+++ reservoir_ESN/script/NL_L_split2.py
@@ -15,12 +15,13 @@ for path in args.data:
     for NL_type in args.NL_types:
         for task in args.taskes:
             fig = plt.figure()
-            ax1 = fig.add_subplot(1, 4, 1)
-            ax2 = fig.add_subplot(1, 4, 2)
-            ax3 = fig.add_subplot(1, 4, 3)
-            ax4 = fig.add_subplot(1, 4, 4)
+            ax1 = fig.add_subplot(1, 5, 1)
+            ax2 = fig.add_subplot(1, 5, 2)
+            ax3 = fig.add_subplot(1, 5, 3)
+            ax4 = fig.add_subplot(1, 5, 4)
+            ax5 = fig.add_subplot(1, 5, 5)
             df = pd.read_csv(args.root + path + '.csv', sep=',',comment='#')
-            for function_name, bias, ax in [("sinc", True, ax1), ("tanh", True, ax2), ("sinc", False, ax3),("tanh", False, ax4)]:
+            for function_name, bias, ax in [("all", True, ax1), ("sinc", True, ax2), ("tanh", True, ax3), ("sinc", False, ax4),("tanh", False, ax5)]:
                 print(path, NL_type, task)
                 df2 = df[df["function_name"] == function_name]
                 if bias:
@@ -28,9 +29,14 @@ for path in args.data:
                 else:
                     df2 = df2[df2["bias_factor"] == 0]
                 #df2.to_csv(task + ".csv")
+                if function_name == "all":
+                    df2 = df
                 print(df2)
                 data_x = df2['L_cut']
-                data_y = df2[NL_type]
+                if NL_type == "NL_test":
+                    data_y = df2["NL_old_2"] + df2["NL_old_3"] + df2["NL_old_4"] + df2["NL_old_5"] + df2["NL_old_6"]
+                else:
+                    data_y = df2[NL_type]
                 value = df2[task]
                 mappable = ax.scatter(data_x, data_y,s=1, marker=".", c = value, alpha=1, linewidths=1, norm=LogNorm(vmin=0.05, vmax=1.0), cmap='viridis_r')
                 ax.set_ylim(0.0, 500)
